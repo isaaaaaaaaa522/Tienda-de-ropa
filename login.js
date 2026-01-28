077e8af9d74da657802af44657be3e4f82af7c83
isabel <olazabalisabel53@gmail.com>
2026-01-28 00:04:24 -0500

    ajax me jode

--- login.js
+++ login.js
@@ -38,10 +38,33 @@ function loadRegister() {
       // 🔑 REINICIALIZAR
       initLogin();
       AbrirCerrar();
+      initFormulario();
     });
 }
+
+ function initFormulario() {
+    const form = document.querySelector(".formulario");
+    const checkbox = document.getElementById("politica");
+    const errorPolitica = document.getElementById("error-politica");
+
+    if (!form || !checkbox || !errorPolitica) return;
+
+    form.addEventListener("submit", e => {
+      if (!checkbox.checked) {
+        e.preventDefault();
+        errorPolitica.style.display = "flex";
+      }
+    });
+
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        errorPolitica.style.display = "none";
+      }
+    });
+  }
 
 
 
   initLogin();
   AbrirCerrar();
+initFormulario();
