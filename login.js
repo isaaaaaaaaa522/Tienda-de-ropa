4708b6917aad0dbc775df3298413c7bdb4c66d4f
isabel <olazabalisabel53@gmail.com>
2026-02-03 23:22:56 -0500

    login en proceso - sin ajax

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 
 /* Ojo de contraseña */
-function AbrirCerrar() {
+/* function AbrirCerrar() {
     const passwordInput = document.getElementById("password");
 const togglePassword = document.getElementById("togglePassword");
 
@@ -13,58 +13,13 @@ togglePassword.addEventListener("click", () => {
   togglePassword.classList.toggle("fa-eye-slash", isPassword);
 });
 }
+ */
 
 
 
 
-/* REGISTER */
-function initLogin() {
-  const btnRegister = document.getElementById("goRegister");
-
-  if (!btnRegister) return;
-
-  btnRegister.addEventListener("click", (e) => {
-    e.preventDefault();
-    loadRegister();
-  });
-}
-
-function loadRegister() {
-  fetch("register.html")
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("content").innerHTML = html;
-
-      // 🔑 REINICIALIZAR
-      initLogin();
-      AbrirCerrar();
-      initFormulario();
-    });
-}
-
- function initFormulario() {
-    const form = document.querySelector(".formulario");
-    const checkbox = document.getElementById("politica");
-    const errorPolitica = document.getElementById("error-politica");
-
-    if (!form || !checkbox || !errorPolitica) return;
-
-    form.addEventListener("submit", e => {
-      if (!checkbox.checked) {
-        e.preventDefault();
-        errorPolitica.style.display = "flex";
-      }
-    });
-
-    checkbox.addEventListener("change", () => {
-      if (checkbox.checked) {
-        errorPolitica.style.display = "none";
-      }
-    });
-  }
 
 
 
-  initLogin();
+
   AbrirCerrar();
-initFormulario();
